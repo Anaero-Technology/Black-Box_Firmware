03987ca9b811f0187d59c7c2b7e0581111cff814
Robbie Goldman <goldmanrobbie@anaero.co.uk>
2026-03-19 14:57:13 +0000

    [network] Added setting up and connecting to wifi network and sending messages via POST.

--- src/modules/network/network_handler.cpp
+++ src/modules/network/network_handler.cpp
@@ -1,5 +1,6 @@
 #include <modules/network/network_handler.h>
 
+extern RTC_Handler rtc;
 
 void Network_Handler::store_mac_address() {
   unsigned char mac[6] = {};
@@ -13,4 +14,76 @@ void Network_Handler::store_mac_address() {
 
 char* Network_Handler::get_mac_address() {
     return mac_address;
+}
+
+bool Network_Handler::set_wifi_info(const char* SSID, const char* PASS) {
+    if (strlen(SSID) < 99) {
+        strcpy(wifi_ssid, SSID);
+    } else {
+        return false;
+    }
+    if (strlen(PASS) < 99) {
+        strcpy(wifi_password, PASS);
+    } else {
+        return false;
+    }
+    return true;
+}
+
+bool Network_Handler::connect_to_wifi() {
+    if (connected_to_wifi) {
+        WiFi.disconnect();
+    }
+    WiFi.begin(wifi_ssid, wifi_password);
+    bool connected = false;
+    bool timed_out = false;
+    unsigned long start = millis();
+    while (!connected && !timed_out) {
+        if (WiFi.status() == WL_CONNECTED) {
+            connected = true;
+        }
+        if (rtc.check_elapsed(start, millis(), connection_timeout)) {
+            timed_out = true;
+        }
+    }
+
+    if (!connected) {
+        WiFi.disconnect();
+    }
+
+    connected_to_wifi = connected;
+
+    return connected_to_wifi;
+}
+
+bool Network_Handler::set_host_address(const char* ADDRESS) {
+    if (strlen(ADDRESS) < 99) {
+        strcpy(host_address, ADDRESS);
+        return true;
+    }
+    return false;
+}
+
+bool Network_Handler::send_message(const char* DATA) {
+    if (connected_to_wifi && strlen(DATA) < 99) {
+        char full_data[230];
+        strcpy(full_data, host_address);
+        //Delimeter
+        strcat(full_data, mac_address);
+        //Delimeter
+        strcat(full_data, DATA);
+        HTTPClient http;
+        http.begin(full_data);
+        int http_response = http.GET();
+        http.end();
+        if (http_response < 0) {
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+bool Network_Handler::is_wifi_connected() {
+    return connected_to_wifi;
 }
--- src/modules/network/network_handler.h
+++ src/modules/network/network_handler.h
@@ -1,10 +1,24 @@
 #pragma once
 #include "esp_mac.h"
+#include "Arduino.h"
+#include "WiFi.h"
+#include "HTTPClient.h"
+#include "modules/rtc/rtc_handler.h"
 
 class Network_Handler {
     public:
         void store_mac_address();
         char* get_mac_address();
+        bool set_wifi_info(const char* SSID, const char* PASS);
+        bool connect_to_wifi();
+        bool set_host_address(const char* ADDRESS);
+        bool send_message(const char* DATA);
+        bool is_wifi_connected();;
     private:
         char mac_address[20];
+        char wifi_ssid[100];
+        char wifi_password[100];
+        bool connected_to_wifi = false;
+        char host_address[100];
+        unsigned long connection_timeout = 10000UL;
 };
