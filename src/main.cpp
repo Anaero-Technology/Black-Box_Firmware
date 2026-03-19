b96984b8ff2e3b273991ee26f60a51c2a0ce6832
Robbie Goldman <goldmanrobbie@anaero.co.uk>
2026-03-19 13:28:24 +0000

    [main] Removed old code and completed module use functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,6 @@
 #include <modules/arduino/arduino_handler.h>
 #include <modules/state/state_handler.h>
 
-/*#include "modules/sd_card/sd_handler.h"
-#include "modules/network/network_handler.h"
-#include "modules/state/state_handler.h"
-#include "modules/rtc/rtc_handler.h"
-#include "modules/serial/serial_handler.h"
-#include "modules/arduino/arduino_handler.h"*/
-
 SD_Handler sd_card;
 Network_Handler network;
 State_Handler state;
@@ -22,11 +15,20 @@ Arduino_Handler arduino;
 bool started_successfully = true;
 
 void arduino_messages() {
-
+    arduino.check_timeout();
+    arduino.receive_characters();
 }
 
 void serial_messages() {
-    
+    serial.receive_characters();
+}
+
+void update_hours() {
+    if (rtc.update_hour_time()) {
+        state.output_tip_counters();
+        sd_card.save_hourly_tips();
+        state.reset_tip_counters();
+    }
 }
 
 void setup() {
@@ -46,68 +48,12 @@ void setup() {
     if (!started_successfully) {
         while (1);
     }
+    Serial.println("Loop Starting");
 }
 
 void loop() {
     arduino_messages();
     serial_messages();
+
+    update_hours();
 }
-
-
-
-/* OLD CODE BELOW */
-
-// void resetTipCounters(){
-//   //Reset the counters for each channel so a new hour can begin
-//   for (int i = 0; i < 15; i = i + 1){
-//     tipCounts[i] = 0;
-//   }
-// }
-
-// void updateHourTime(){
-//   if(collecting){
-//     uint32_t currentTime = millis();
-//     uint32_t elapsed = currentTime - hourStarted;
-//     if (currentTime < hourStarted){
-//       elapsed = (ULONGMAX - hourStarted) + currentTime;
-//     }
-//     if (elapsed >= hourLength){
-//       outputTipCounters();
-//       if(filesWorking){
-//         saveHourTips();
-//       }
-//       resetTipCounters();
-//       hourStarted = hourStarted + hourLength;
-//     }
-//   }
-// }
-
-// void outputTipCounters(){
-//   Serial.write("counts ");
-//   for (int i = 0; i < 15; i = i + 1){
-//     Serial.print(tipCounts[i]);
-//     Serial.write(" ");  
-//   }
-//   Serial.write("\n");
-// }
-
-// void saveHourTips(){
-//   if(!SD.exists(hourlyTipFile)){
-//     File hourlyFile = SD.open(hourlyTipFile, FILE_WRITE);
-//     hourlyFile.close();
-//   }
-//   File hourlyFile = SD.open(hourlyTipFile, FILE_APPEND);
-//   for(int i = 0; i < 15; i = i + 1){
-//     hourlyFile.print(tipCounts[i]);
-//     if(i != 14){
-//       hourlyFile.print(' ');
-//     }
-//   }
-//   hourlyFile.print('\n');
-//   hourlyFile.close();
-// }
-
-// void clearHourTips(){
-//   File hourlyFile = SD.open(hourlyTipFile, FILE_WRITE);
-//   hourlyFile.close();
-// }
